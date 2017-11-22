9ec273a01b93f3e23bad8c88936aa82aa5feb661
Heronides <haplneto@gmail.com>
2017-11-22 14:59:59 -0300

    fui babay

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 
 using namespace std;
-int xubiruba;
 int main()
 {
-    cout << "Hello world!" << endl;
+    GLFWwindow* window;
+    int error = glfwInit();
+    if (error == GLFW_FALSE){
+        cout << "fudeu" << endl;
+    }else{
+        cout << "blzinha" << endl;
+    }
+    // propriedades da janela
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);      //VERSAO DO OPENGL MAIOR QUE :
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);      //VERSAO DO OPENGL MENOR QUE :
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);        //COMPATIVEL COM VERSOES ANTERIORES A 1.1
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);            //POSSIBILIDADE DE REDIMENCIONAR A JANELA PUXANDO
+
+    window = glfwCreateWindow(800,600,"bolsonaro2018",NULL,NULL);
+    if (window == NULL){
+        cout << "fudeu no window" << endl;
+    }
+    glfwMakeContextCurrent(window);     //vincular a opengl com a janela - tudo que desenhar aparecer na janela
+    glfwSwapInterval(1);                //troca de telas pelo opengl
+
+    while(!glfwWindowShouldClose(window)){                 //se fechar a janela
+        glClearColor(0.0f,0.8f,0.0f,1.0f);                  // cor preta para limpar tela ( RGBA )
+        glClear(GL_COLOR_BUFFER_BIT);                       // limpar o buffer com a cor que declarei antes
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+
+
+
     return 0;
-    xubiruba=0;
 }
